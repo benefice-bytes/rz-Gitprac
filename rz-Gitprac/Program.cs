13ae258c8c691139fc3f5455732db470cc62feca
Sazid <sazid.sunny@gmail.com>
2021-11-16 17:08:10 +0530

    Mangoes added

--- rz-Gitprac/Program.cs
+++ rz-Gitprac/Program.cs
@@ -7,6 +7,7 @@ namespace rz_Gitprac
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Mangoes");
         }
     }
 }
