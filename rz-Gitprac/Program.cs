e7c4fdd565a7b53e8d606e3eb81807c0f76acf95
Nazeeruddin <naseers.tech@gmail.com>
2021-11-16 16:56:26 +0530

    Add project files.

---
+++ rz-Gitprac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rz_Gitprac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
